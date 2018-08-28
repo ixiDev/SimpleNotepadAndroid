39fb3ad39f60c779925478f2c1ab3209510288b3
ixiDev <feedback.mrzero@gmail.com>
2018-08-28 16:58:15 +0100

    part 4

--- app/src/main/java/com/ixidev/simplenotepad/callbacks/NoteEventListener.java
+++ app/src/main/java/com/ixidev/simplenotepad/callbacks/NoteEventListener.java
@@ -7,14 +7,14 @@ import com.ixidev.simplenotepad.model.Note;
  */
 public interface NoteEventListener {
     /**
-     * call wen note clicked
+     * call wen note clicked.
      *
      * @param note: note item
      */
     void onNoteClick(Note note);
 
     /**
-     * call wen long Click to note
+     * call wen long Click to note.
      *
      * @param note : item
      */
